0a0aa9cdffeb88185a336b6da51f8b09aaccbebd
shatadalsamui <shatadalsamui@gmail.com>
2025-01-04 09:00:00 +0530

    Initial async JS setup

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const fs = require("fs") ; // fs is external library which lets u read external files
-const contents  = fs.readFileSync("a.txt","utf-8"); //utf-8 is
+const contents  = fs.readFileSync("a.txt","utf-8"); //utf-8 is type
 console.log(contents);
